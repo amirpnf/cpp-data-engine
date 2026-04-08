52e7f8191653a66abf20d486b41e126fdafbddc8
amirh <pouyanfar.a2000@gmail.com>
2026-04-08 09:41:47 +0200

    Extra operations

--- include/operations.h
+++ include/operations.h
@@ -1,5 +1,6 @@
 #include "row.h"
 #include <functional>
+#include <map>
 
 std::vector<Row> map_rows(
     const std::vector<Row>& data,
@@ -21,4 +22,28 @@ std::vector<Row> filter_rows_parallel(
     const std::vector<Row>& data,
     std::function<bool(const Row&)> predicate,
     int num_thread
+);
+
+template<typename T>
+T reduce_rows(
+    const std::vector<Row>& data,
+    std::function<T(T, const Row&)> reducer,
+    T initial 
+);
+
+size_t count_rows(const std::vector<Row>& data);
+
+std::unordered_map<std::string, std::vector<Row>>
+groupby(
+    const std::vector<Row>& data,
+    std::function<std::string(const Row&)> key_func
+);
+
+std::vector<Row> sort_rows(
+    const std::vector<Row>& data,
+    std::function<bool(const Row&, const Row&)> comp
+);
+
+std::vector<Row> distinct_row(
+    const std::vector<Row>& data
 );
