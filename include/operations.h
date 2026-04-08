af1257119c9c3df6aebefabc675a58f2b20bd182
amirh <pouyanfar.a2000@gmail.com>
2026-04-08 11:53:37 +0200

    reduce_rows sequential and parallel

--- include/operations.h
+++ include/operations.h
@@ -28,8 +28,17 @@ template<typename T>
 T reduce_rows(
     const std::vector<Row>& data,
     std::function<T(T, const Row&)> reducer,
-    T initial 
+    T initial
 );
+
+template<typename T>
+T reduce_rows_parallel(
+    const std::vector<Row>& data,
+    std::function<T(T, const Row&)> reducer,
+    std::function<T(T, T)> combine,
+    T initial,
+    int num_thread=1
+); 
 
 size_t count_rows(const std::vector<Row>& data);
 
--- src/operations.cpp
+++ src/operations.cpp
@@ -62,11 +62,12 @@ std::vector<Row> filter_rows_parallel(
     std::function<bool(const Row&)> predicate,
     int num_thread
 ) {
-    std::vector<std::thread> threads;
-    std::vector<std::vector<Row>> local_results(num_thread);
-
+    
     if(num_thread <= 0) num_thread = 1;
     num_thread = std::min(num_thread, (int)data.size());
+
+    std::vector<std::thread> threads;
+    std::vector<std::vector<Row>> local_results(num_thread);
 
     int chunk_size = data.size() / num_thread;
     for(int i = 0; i < num_thread; i++) {
@@ -89,6 +90,62 @@ std::vector<Row> filter_rows_parallel(
     std::vector<Row> result;
     for(const auto& local : local_results) {
         result.insert(result.end(), local.begin(), local.end());
+    }
+    return result;
+}
+
+template<typename T>
+T reduce_rows(
+    const std::vector<Row>& data,
+    std::function<T(T, const Row&)> reducer,
+    T initial
+) {
+
+    T acc = initial;
+    for(const auto& val : data) {
+        acc = reducer(acc, val);
+    }
+    return acc;
+}
+
+template<typename T>
+T reduce_rows_parallel(
+    const std::vector<Row>& data,
+    std::function<T(T, const Row&)> reducer,
+    std::function<T(T, T)> combine,
+    T initial,
+    int num_thread=1
+) {
+
+    if(data.empty()) return initial;
+
+    num_thread = std::min(num_thread, (int)data.size());
+
+    std::vector<std::thread> threads;
+    std::vector<T> partial_results(num_thread, initial);
+
+    int chunk_size = data.size() / num_thread;
+    
+    for(int i = 0; i < num_thread; i++) {
+        int start = i * chunk_size;
+        int end = (i == num_thread - 1) ? data.size() : start + chunk_size;
+
+        threads.emplace_back([&, start, end, i]() {
+            T acc = initial;
+            for(int j = start; j < end; j++) {
+                acc = reducer(acc, data[j]);
+            }
+            partial_results[i] = acc;
+        });
+    }
+
+    for(auto& t : threads) {
+        t.join();
+    }
+
+    T result = initial;
+    for(const T& val : partial_results) {
+        result = combine(result, val);
     }
     return result;
 }
